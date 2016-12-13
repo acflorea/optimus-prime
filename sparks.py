699cfc1b6b1c44436b8cf34d7eedfd8c882cffd3
acflorea <acflorea@gmail.com>
2016-12-13 22:50:22 +0200

    Add .gitignore

--- sparks.py
+++ sparks.py
@@ -1,17 +1,19 @@
 from subprocess import call
 
-print "We begin..."
+def targetFn():
 
-resultsFileName = 'results.out'
-root = '/Users/acflorea/phd/columbugus_data/netbeans_final_test'
+    print "We begin..."
 
-sparkParams = '-Dconfig.file=/Users/acflorea/Bin/spark-1.6.2-bin-hadoop2.6/columbugus-conf/netbeans.conf ' \
+    resultsFileName = 'results.out'
+    root = '/Users/acflorea/phd/columbugus_data/netbeans_final_test'
+
+    sparkParams = '-Dconfig.file=/Users/acflorea/Bin/spark-1.6.2-bin-hadoop2.6/columbugus-conf/netbeans.conf ' \
               '-Dreccsys.phases.preprocess=false ' \
               '-Dreccsys.preprocess.includeCategory=false ' \
               '-Dreccsys.preprocess.includeProduct=false ' \
               '-Dreccsys.filesystem.resultsFileName=' + resultsFileName + ' -Dreccsys.filesystem.root=' + root
 
-call(['/Users/acflorea/Bin/spark-1.6.2-bin-hadoop2.6/bin/spark-submit',
+    call(['/Users/acflorea/Bin/spark-1.6.2-bin-hadoop2.6/bin/spark-submit',
       '--class', 'dr.acf.recc.ReccomenderBackbone',
       '--master=local[*]',
       '--executor-memory', '20G',
@@ -21,8 +23,11 @@ call(['/Users/acflorea/Bin/spark-1.6.2-bin-hadoop2.6/bin/spark-submit',
       sparkParams,
       '/Users/acflorea/phd/columbugus/target/scala-2.10/columbugus-assembly-2.1.jar'])
 
-f = open(root + "/" + resultsFileName, 'r')
-results = f.read()
-print results
+    f = open(root + "/" + resultsFileName, 'r')
+    results = f.read()
+    print results
 
-print "And we're done!"
+    print "And we're done!"
+
+# Go!
+targetFn()
