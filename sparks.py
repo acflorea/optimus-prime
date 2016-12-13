0ffc96db24554b2b595a537d5a53eb6e3641b77d
acflorea <acflorea@gmail.com>
2016-12-13 22:42:48 +0200

    Initial commit

---
+++ sparks.py
@@ -0,0 +1,28 @@
+from subprocess import call
+
+print "We begin..."
+
+resultsFileName = 'results.out'
+root = '/Users/acflorea/phd/columbugus_data/netbeans_final_test'
+
+sparkParams = '-Dconfig.file=/Users/acflorea/Bin/spark-1.6.2-bin-hadoop2.6/columbugus-conf/netbeans.conf ' \
+              '-Dreccsys.phases.preprocess=false ' \
+              '-Dreccsys.preprocess.includeCategory=false ' \
+              '-Dreccsys.preprocess.includeProduct=false ' \
+              '-Dreccsys.filesystem.resultsFileName=' + resultsFileName + ' -Dreccsys.filesystem.root=' + root
+
+call(['/Users/acflorea/Bin/spark-1.6.2-bin-hadoop2.6/bin/spark-submit',
+      '--class', 'dr.acf.recc.ReccomenderBackbone',
+      '--master=local[*]',
+      '--executor-memory', '20G',
+      '--driver-java-options',
+      '-Xmx32G',
+      '--driver-java-options',
+      sparkParams,
+      '/Users/acflorea/phd/columbugus/target/scala-2.10/columbugus-assembly-2.1.jar'])
+
+f = open(root + "/" + resultsFileName, 'r')
+results = f.read()
+print results
+
+print "And we're done!"
