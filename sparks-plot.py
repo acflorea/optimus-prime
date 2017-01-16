737df2c3df1ea364f93a9ad6b472a97f01ba46a4
acflorea <acflorea@gmail.com>
2017-01-16 19:21:32 +0200

    Plot results

---
+++ sparks-plot.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from matplotlib import pylab as plt
+from mpl_toolkits.mplot3d import axes3d, Axes3D  # <-- Note the capitalization!
+
+dataset = "netbeans"
+
+# Particle Swarm
+df = pd.read_csv('data/' + dataset + '/ps_out.csv')
+
+# Nelder-Mead
+# df = pd.read_csv('data/' + dataset + '/nm_out.csv')
+
+# Random search
+# df = pd.read_csv('data/' + dataset + '/rnd_out.csv')
+
+# Grid search
+# df = pd.read_csv('data/' + dataset + '/g_out.csv')
+
+cutoff = 0.5
+fig = plt.figure()
+ax = Axes3D(fig)
+
+ax.scatter(xs=df['categoryScalingFactor'],
+           ys=df['productScalingFactor'],
+           zs=df['value'])
+
+# ax.scatter(xs=df[df.value > cutoff]['categoryScalingFactor'],
+#            ys=df[df.value > cutoff]['productScalingFactor'],
+#            zs=df[df.value > cutoff]['value'])
+
+ax.set_xlabel('categoryScalingFactor')
+ax.set_ylabel('productScalingFactor')
+ax.set_zlabel('F1')
+
+plt.show()
