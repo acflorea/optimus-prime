b23de4c724d3c6622766c0bcb94a679217dee80e
acflorea <acflorea@gmail.com>
2017-08-01 21:40:33 +0300

    Add crossVal script

---
+++ crossVal.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_svmlight_file
+from sklearn.cross_validation import cross_val_score
+from sklearn import svm
+import sys
+
+def main(args):
+    dataset = load_svmlight_file("/Users/acflorea/phd/libsvm-datasets/adult/a1a.libsvm")
+
+    X_train = dataset[0].todense()
+    y_train = dataset[1]
+
+    clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
+
+    scores = cross_val_score(clf, X_train, y_train, cv=10)
+
+    return scores
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main(sys.argv)
--- optunity_libsvm.py
+++ optunity_libsvm.py
@@ -2,33 +2,34 @@ import optunity
 import optunity.metrics
 
 import sklearn.svm
-import numpy as np
 
-from sklearn.datasets import load_digits
+from sklearn.datasets import load_digits, load_svmlight_file
 
-digits = load_digits()
-n = digits.data.shape[0]
+import time
 
-positive_digit = 8
-negative_digit = 9
+dataset = load_svmlight_file("/Users/acflorea/phd/libsvm-datasets/adult/a1a.libsvm")
 
-positive_idx = [i for i in range(n) if digits.target[i] == positive_digit]
-negative_idx = [i for i in range(n) if digits.target[i] == negative_digit]
+n = dataset[1].size
+
+positive_label = 1.0
+negative_lable = -1.0
+
+positive_idx = [i for i in range(n) if dataset[1][i] == positive_label]
+negative_idx = [i for i in range(n) if dataset[1][i] == negative_lable]
 
 # add some noise to the data to make it a little challenging
-original_data = digits.data[positive_idx + negative_idx, ...]
-data = original_data + 5 * np.random.randn(original_data.shape[0], original_data.shape[1])
 labels = [True] * len(positive_idx) + [False] * len(negative_idx)
-
 
 # we will make the cross-validation decorator once, so we can reuse it later for the other tuning task
 # by reusing the decorator, we get the same folds etc.
-cv_decorator = optunity.cross_validated(x=data, y=labels, num_folds=10)
+cv_decorator = optunity.cross_validated(x=dataset[0].todense(), y=dataset[1], num_folds=10)
+
 
 def svm_rbf_tuned_acc(x_train, y_train, x_test, y_test, C, logGamma):
     model = sklearn.svm.SVC(C=C, gamma=10 ** logGamma).fit(x_train, y_train)
     y_hat = model.predict(x_test)
     acc = optunity.metrics.accuracy(y_test, y_hat)
+    print (C, logGamma, acc)
     return acc
 
 
@@ -37,10 +38,12 @@ svm_rbf_tuned_acc = cv_decorator(svm_rbf_tuned_acc)
 # @optunity.cross_validated(x=data, y=labels, num_folds=5)
 # def svm_rbf_tuned_auroc...
 
-print svm_rbf_tuned_acc(C=4.633138020833334, logGamma=-2.8818894810828026)
+start_time = time.time()
 
 optimal_rbf_pars, info, _ = optunity.maximize(svm_rbf_tuned_acc, num_evals=150, C=[0, 10], logGamma=[-5, 0],
                                               pmap=optunity.pmap)
 
 print("Optimal parameters: " + str(optimal_rbf_pars))
 print("ACC of tuned SVM with RBF kernel: %1.3f" % info.optimum)
+
+print("--- %s seconds ---" % (time.time() - start_time))
