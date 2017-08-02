3f6511fe6c24eac9d293e1f5faa6d467a85a9e66
Adrian Florea <acflorea@gmail.com>
2017-08-02 17:33:00 +0300

    Enhance scripts to take params and return values

--- crossVal.py
+++ crossVal.py
@@ -4,22 +4,27 @@ from sklearn import svm
 import sys
 import time
 
+
 def main(args):
-    dataset = load_svmlight_file("/Users/aflorea/phd/libsvm-datasets/adult/a1a.t.libsvm")
+    fileName = args[1]
+
+    dataset = load_svmlight_file(fileName)
 
     start_time = time.time()
 
     X_train = dataset[0].todense()
     y_train = dataset[1]
 
-    clf = svm.SVC(kernel='linear', C=1) #.fit(X_train, y_train)
+    clf = svm.SVC(kernel='linear', C=1)  # .fit(X_train, y_train)
 
     scores = cross_val_score(clf, X_train, y_train, cv=10)
 
     print("--- %s seconds ---" % (time.time() - start_time))
-    print scores
 
-    sys.exit(scores)
+    # calculate stuff
+    sys.stdout.write(str(scores))
+    sys.stdout.flush()
+    sys.exit(0)
 
 
 if __name__ == "__main__":
