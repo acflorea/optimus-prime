f6e64bf5f6e411394dbff3001ce9e1edb45681e1
Adrian Florea <acflorea@gmail.com>
2017-09-01 17:26:06 +0300

    More on batch mode

--- batchExecuter.py
+++ batchExecuter.py
@@ -1,18 +1,45 @@
 import sys
+from subprocess import call
 
 
 def main(args):
+    # /Users/aflorea/phd/optimus-prime/optunity_libsvm.py
+    executor = args[1]
+
+    # /Users/aflorea/phd/optimus-prime/crossVal.py
+    script = args[2]
 
     # /Users/aflorea/phd/libsvm-datasets
-    datasetsRootFolder = args[1]
+    datasetsRootFolder = args[3]
 
-    datasets = ['/adult/a6a.libsvm',
-                'cancer/breast-cancer_scale.libsvm',
+    datasets = ['/adult/a1a.libsvm',
+                '/cancer/breast-cancer_scale.libsvm',
                 '/diabetes/diabetes_scale.libsvm',
                 '/iris/iris.libsvm',
                 '/poker/poker.libsvm',
-                'wine/wine.libsvm'
+                '/wine/wine.libsvm'
                 ]
+
+    for dataset in datasets:
+        # Build the output file name
+        outputFileName = datasetsRootFolder + dataset + '.log'
+
+        # python
+        # optunity_libsvm.py
+        # "/Users/aflorea/phd/optimus-prime/crossVal.py" "/Users/aflorea/phd/libsvm-datasets/adult/a1a.libsvm"
+        # 250
+        # "random search"
+
+        call(["python",
+              executor,
+              script,
+              datasetsRootFolder + dataset,
+              "250",
+              "random search"
+              ],
+             stdout=open(outputFileName, 'w'),
+             stderr=open(outputFileName, 'w'),
+             cwd=datasetsRootFolder)
 
 
 if __name__ == "__main__":
